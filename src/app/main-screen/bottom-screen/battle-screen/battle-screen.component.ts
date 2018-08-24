a8e4cd173d0075f22cee964c2a135e1b16d17cd0
David Jiang <djiang86@binghamton.edu>
2018-08-24 21:12:28 +0000

    button layout in battle screen

--- src/app/main-screen/bottom-screen/battle-screen/battle-screen.component.ts
+++ src/app/main-screen/bottom-screen/battle-screen/battle-screen.component.ts
@@ -6,7 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./battle-screen.component.css']
 })
 export class BattleScreenComponent implements OnInit {
-
+  skills: any = [
+    {
+      skillName: 'Hypnosis',
+      type: 'Psychic',
+      ppTotal: 10,
+      ppLeft: 5
+    },
+    {
+      skillName: 'Absorb',
+      type: 'Grass',
+      ppTotal: 25,
+      ppLeft: 10
+    },
+    {
+      skillName: 'Acid',
+      type: 'Poison',
+      ppTotal: 30,
+      ppLeft: 18
+    },
+    {
+      skillName: 'Accelerock',
+      type: 'Rock',
+      ppTotal: 20,
+      ppLeft: 18
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
